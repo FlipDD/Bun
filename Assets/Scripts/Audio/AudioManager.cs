75086eecf29d6d702813f697e500cf7209adfb33
flipdd <flipddr@gmail.com>
2020-01-30 02:40:26 +0000

    First Bun

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the audio to play and its volume
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public List<Sound> soundList;
+
+    private Dictionary<string, Sound> sounds;
+
+    public float sfxMultiplier = 1;
+    public float musicMultiplier = 1;
+
+    private Sound musicPlaying;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        foreach (var s in soundList)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+        }
+    }
+
+    private void Start()
+    {
+        sounds = new Dictionary<string, Sound>();
+        foreach (var sound in soundList)
+        {
+            sounds.Add(sound.name, sound);
+        }
+
+        Play("Background", musicMultiplier, true);
+    }
+
+    public void Play(string name, float multiplier, bool looping = false)
+    {
+        if (sounds.TryGetValue(name, out Sound s))
+        {
+            s.source.volume = multiplier;
+            s.source.loop = looping;
+            s.source.Play();
+        }
+    }
+
+    public void Stop(string name)
+    {
+        if (sounds.TryGetValue(name, out Sound s))
+            s.source.Stop();
+    }
+
+    public void SetMusic(float value)
+    {
+        musicMultiplier = value;
+        if (musicPlaying != null)
+        {
+            musicPlaying.source.volume = musicMultiplier;
+        }
+    }
+
+    public void SetSfx(float value)
+    {
+        sfxMultiplier = value;
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuFunctionality.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handler for the main menu
+/// </summary>
+public class MainMenuFunctionality : MonoBehaviour
+{
+    [SerializeField]
+    GameObject MainMenu = null;
+    [SerializeField]
+    GameObject SettingsMenu = null;
+
+    public void StartGame() => SceneManager.LoadScene(1);
+
+    public void QuitGame() => Application.Quit();
+
+    public void ShowSettingsMenu()
+    {
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        SettingsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+}
